dfc4195057d44c7e223639774e65b6bb2ae21913
Kat Gaea <kat.knight@newgaea.net>
2024-03-23 14:16:41 +0000

    feat: Add map

--- engine.py
+++ engine.py
@@ -6,6 +6,7 @@ from tcod.console import Console
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
+from tcod.map import compute_fov
 
 
 class Engine:
@@ -14,6 +15,7 @@ class Engine:
         self.event_handler = event_handler
         self.player = player
         self.game_map = game_map
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -22,9 +24,21 @@ class Engine:
                 continue
             action.perform(self,self.player)
 
+            self.update_fov()
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            if self.game_map.visible[entity.x,entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
         context.present(console)
         console.clear()
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:]= compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x,self.player.y),
+            radius=8
+        )
+        self.game_map.explored |= self.game_map.visible
